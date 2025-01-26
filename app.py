5c41e729c9e9fae89c29e4f2861c63ab8249ed28
Jake Alstad <jake.alstad@gmail.com>
2025-01-25 21:41:17 -0600

    disable analytics in development

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html", debug=app.debug)
+
 
 @app.route("/resume/")
 def resume():
-	return render_template("resume.html")
+    return render_template("resume.html")
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
